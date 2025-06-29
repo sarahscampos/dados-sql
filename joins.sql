0d480cf48b7da09e380a04650e67d139c7bf8ae8
Sarah Campos <sarahstephanycampos@gmail.com>
2025-06-29 12:32:06 -0300

    joins

---
+++ joins.sql
@@ -0,0 +1,30 @@
+-- EXERCÍCIOS ########################################################################
+
+-- (Exercício 1) Identifique quais as marcas de veículo mais visitada na tabela sales.funnel
+
+select prod.brand, count(fun.visit_id) as qtd_visitas
+from sales.funnel as fun
+left join sales.products as prod	
+	on fun.product_id = prod.product_id
+group by prod.brand
+order by qtd_visitas desc
+
+-- (Exercício 2) Identifique quais as lojas de veículo mais visitadas na tabela sales.funnel
+
+select sto.store_name, count(fun.visit_id) as qtd_visitas
+from sales.funnel as fun
+left join sales.stores as sto
+	on fun.store_id = sto.store_id
+group by sto.store_name
+order by qtd_visitas desc
+
+-- (Exercício 3) Identifique quantos clientes moram em cada tamanho de cidade (o porte da cidade
+-- consta na coluna "size" da tabela temp_tables.regions)
+select reg.size, count(cus.customer_id) qtd_clientes
+from sales.customers as cus
+left join temp_tables.regions as reg
+	on lower(cus.state) = lower(reg.state) and lower(cus.city) = lower(reg.city)
+group by reg.size
+order by qtd_clientes desc
+
+	
