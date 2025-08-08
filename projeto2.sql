9da2078413951257e9e0091d0d846e23aea984d0
Sarah Campos <74733887+sarahscampos@users.noreply.github.com>
2025-08-08 18:16:31 -0300

    Add files via upload

---
+++ projeto2.sql
@@ -0,0 +1,164 @@
+select
+	case
+		when ibge.gender = 'male' then 'homens'
+		when ibge.gender = 'female' then 'mulheres'
+		end as "gênero",
+	count(*) as "leads"
+
+from sales.customers as cus
+left join temp_tables.ibge_genders as ibge
+	on lower(cus.first_name) = lower(ibge.first_name)
+group by ibge.gender
+
+--
+
+select
+	case
+		when professional_status = 'freelancer' then 'freelancer'
+		when professional_status = 'retired' then 'aposentado'
+		when professional_status = 'clt' then 'clt'
+		when professional_status = 'self_employed' then 'autônomo(a)'
+		when professional_status = 'other' then 'outro'
+		when professional_status = 'businessman' then 'empresário(a)'
+		when professional_status = 'civil_servant' then 'funcionário(a) público(a)'
+		when professional_status = 'student' then 'estudante'
+		end as "status profissional",
+	(count(*)::float)/(select count(*) from sales.customers) as "leads (%)"
+	
+from sales.customers
+group by professional_status
+order by "leads (%)"
+
+--
+
+create function datediff(unidade varchar, data_inicial date, data_final date)
+returns integer
+language sql
+
+as
+
+$$
+
+	select
+		case
+			when unidade in ('d', 'day', 'days') then (data_final - data_inicial)
+			when unidade in ('w', 'week', 'weeks') then (data_final - data_inicial)/7
+			when unidade in ('m', 'month', 'months') then (data_final - data_inicial)/30
+			when unidade in ('y', 'year', 'years') then (data_final - data_inicial)/365
+			end as diferenca
+
+$$
+
+select
+	case
+		when datediff('years', birth_date, current_date) < 20 then '0-20'
+		when datediff('years', birth_date, current_date) < 40 then '20-40'
+		when datediff('years', birth_date, current_date) < 60 then '40-60'
+		when datediff('years', birth_date, current_date) < 80 then '60-80'
+		else '80+' end as "faixa etária",
+	count(*)::float/(select count(*) from sales.customers) as "leads (%)"
+
+from sales.customers
+group by "faixa etária"
+order by "faixa etária" desc
+
+--
+
+select
+	case
+		when income < 5000 then '0-5000'
+		when income < 10000 then '5000-10000'
+		when income < 15000 then '10000-15000'
+		when income < 20000 then '15000-20000'
+		else '20000+' end as "faixa salarial",
+	count(*)::float/(select count(*) from sales.customers) as "leads (%)",
+	case
+		when income < 5000 then '1'
+		when income < 10000 then '2'
+		when income < 15000 then '3'
+		when income < 20000 then '4'
+		else '5' end as "ordem"
+
+from sales.customers
+group by "faixa salarial", "ordem"
+order by "ordem" desc
+
+--
+
+with 
+	classificacao_veiculos as (
+		select
+			fun.visit_page_date,
+			pro.model_year,
+			extract('year' from visit_page_date) - pro.model_year::int as idade_veiculo,
+			case
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=2 then 'novo'
+				else 'seminovo'
+				end as "classificação do veículo"
+
+			from sales.funnel as fun
+			left join sales.products as pro
+				on fun.product_id = pro.product_id
+	)
+
+
+select
+	"classificação do veículo",
+	count(*) as "veículos visitados (#)"
+
+from classificacao_veiculos
+group by "classificação do veículo"
+
+--
+
+with 
+	faixa_de_idade_dos_veiculos as (
+		select
+			fun.visit_page_date,
+			pro.model_year,
+			extract('year' from visit_page_date) - pro.model_year::int as idade_veiculo,
+			case
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=2 then 'até 2 anos'
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=4 then 'de 2 à 4 anos'
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=6 then 'de 4 à 6 anos'
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=8 then 'de 6 à 8 anos'
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=10 then 'de 8 à 10 anos'
+				else 'acima de 10 anos'
+				end as "idade do veículo",
+			case
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=2 then '1'
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=4 then '2'
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=6 then '3'
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=8 then '4'
+				when (extract('year' from visit_page_date) - pro.model_year::int)<=10 then '5'
+				else '6'
+				end as "ordem"
+
+			from sales.funnel as fun
+			left join sales.products as pro
+				on fun.product_id = pro.product_id
+	)
+
+
+select
+	"idade do veículo",
+	count(*)::float/(select count(*) from sales.funnel) as "veículos visitados (%)",
+	"ordem"
+
+from faixa_de_idade_dos_veiculos
+group by "idade do veículo", "ordem"
+order by "ordem"
+
+--
+
+select 
+	pro.brand, 
+	pro.model,
+	count(*) as "visitas (#)"
+
+from sales.funnel as fun
+left join sales.products as pro
+	on fun.product_id = pro.product_id
+group by pro.brand, pro.model
+order by pro.brand, pro.model, "visitas (#)"
+
